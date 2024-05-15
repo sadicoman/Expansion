6deac217337a05bd3985e9a49a7f08c7e8cbe933
Francois Szczepkowski <francois.szcz@hotmail.fr>
2024-05-15 21:59:46 +0200

    UPDATE : responsive + btn anim

--- developpement/Expansion/src/components/Content/Content.tsx
+++ developpement/Expansion/src/components/Content/Content.tsx
@@ -1,11 +1,56 @@
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
 import LinkButton from "../Common/LinkButton/LinkButton";
 import "./Content.scss";
 
-function Content() {
+const Content: React.FC = () => {
+	const container = useRef(null);
+	const titleRef = useRef(null);
+
+	useGSAP(
+		() => {
+			gsap.registerPlugin(SplitText);
+
+			const splitTitle = new SplitText(titleRef.current, { type: "words, chars" });
+			const chars = splitTitle.chars;
+
+			const tl = gsap.timeline();
+			tl.from(chars, {
+				duration: 0.2,
+				opacity: 0,
+				y: -50,
+				ease: "back.out(1.7)",
+				stagger: 0.04,
+				delay: 1.5,
+			});
+
+			gsap.from(".text", {
+				opacity: 0,
+				y: -20,
+				duration: 0.3,
+				delay: 3.5,
+			});
+
+			gsap.from(".box", {
+				opacity: 0,
+				y: -20,
+				duration: 0.4,
+				delay: 4,
+			});
+
+			return () => {
+				splitTitle.revert();
+			};
+		},
+		{ scope: container },
+	);
+
 	return (
 		<>
-			<section className="section">
-				<h1 className="title">
+			<section ref={container} className="section">
+				<h1 ref={titleRef} className="title">
 					Nous vous accompagnons dans votre{" "}
 					<span className="color--primary">expansion digitale</span>
 				</h1>
@@ -24,6 +69,6 @@ function Content() {
 			</section>
 		</>
 	);
-}
+};
 
 export default Content;
